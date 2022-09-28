0b8ac32b5fe0564ac436c6b0f0f3b594d91943d1
张晓华 <88888888zxh@gmail.com>
2022-09-28 13:10:37 +0800

    Project initialized first

---
+++ gulimall-product/src/test/java/cloud/huel/mall/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.huel.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ gulimall-ware/src/test/java/cloud/huel/mall/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.huel.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
